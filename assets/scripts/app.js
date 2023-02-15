9081254b838f9b4a9a7613a261c33b8667f8659a
NhungTranPY <nhungtranpy91@gmail.com>
2023-02-15 17:28:32 +0700

    370. JSON Data & Parsing Data

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,25 @@
+const listElement = document.querySelector('.posts') // select element with class 'posts'
+const postTemplate = document.getElementById('single-post')
+
 const xhr = new XMLHttpRequest()
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
 
+xhr.responseType = 'json' // ~ JSON.parse(xhr.response)
+
+xhr.onload = function() {
+    // console.log(xhr.response);
+    // const listOfPosts = JSON.parse(xhr.response)
+    const listOfPosts = xhr.response
+    console.log(listOfPosts);
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true)
+        postEl.querySelector('h2').textContent = post.title.toUpperCase()
+        postEl.querySelector('p').textContent = post.body
+        listElement.append(postEl)
+    }
+}
+
 xhr.send()
+
+
