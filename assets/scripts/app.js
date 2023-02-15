618d3af329327bc52c32500de6eb8b5692a67db8
NhungTranPY <nhungtranpy91@gmail.com>
2023-02-15 20:54:25 +0700

    Promisifying Http Requests (with XMLHttpRequest)

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,16 +1,28 @@
 const listElement = document.querySelector('.posts') // select element with class 'posts'
 const postTemplate = document.getElementById('single-post')
 
-const xhr = new XMLHttpRequest()
+function sendHttpRequest(method, url) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
 
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
+        xhr.open(method, url)
 
-xhr.responseType = 'json' // ~ JSON.parse(xhr.response)
+        xhr.responseType = 'json' // ~ JSON.parse(xhr.response)
 
-xhr.onload = function() {
-    // console.log(xhr.response);
+        xhr.onload = function() {
+            resolve(xhr.response)
+            // console.log(xhr.response);
+        }
+
+        xhr.send()
+    })
+    return promise
+}
+
+async function fetchPosts() {
+    const responseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts')
     // const listOfPosts = JSON.parse(xhr.response)
-    const listOfPosts = xhr.response
+    const listOfPosts = responseData
     console.log(listOfPosts);
     for (const post of listOfPosts) {
         const postEl = document.importNode(postTemplate.content, true)
@@ -20,6 +32,5 @@ xhr.onload = function() {
     }
 }
 
-xhr.send()
-
+fetchPosts()
 
